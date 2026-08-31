6775b912798b4a326abcfa250468b18c732a9a30
Pamela <atencionironbody@gmail.com>
2026-08-31 01:12:33 -0400

    feat(catalog): trigger seed_catalog in docker entrypoint on container startup

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 # Run database migrations automatically on startup
 python src/manage.py migrate --noinput
 
+# Idempotently seed initial product catalog on container startup
+python src/manage.py seed_catalog
+
 # Execute container command
 exec "$@"
